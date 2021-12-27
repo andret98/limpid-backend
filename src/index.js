bb14194f78f6259c00ffd2a30f1e01d26a0fc666
George-Andrei IANCU (94825) <george_andrei.iancu@stud.acs.upb.ro>
2021-12-27 21:15:30 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(helmet());
 app.use(morgan(':remote-addr - :remote-user [:date[web]] ":method :url HTTP/:http-version" :status :res[content-length]'));
 app.use(express.json());
+app.use(cors())
 app.use('/',routes);
 
 app.get('/', (req, res) => res.send("da"));
